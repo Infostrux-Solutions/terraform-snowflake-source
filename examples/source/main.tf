8ad37ad436461683e565a31d1582c1cb5425945d
Mprasen <prasen@infostrux.com>
2022-08-18 11:55:32 -0400

    fix: updated README and example

--- examples/source/main.tf
+++ examples/source/main.tf
@@ -20,16 +20,16 @@ module "ingest" {
   ingest_database_id = "DATABASE_EXAMPLE"
 
   ingest_schema_config = {
-    is_managed = "false", 
-    is_transient = "false", 
+    is_managed          = "false",
+    is_transient        = "false",
     data_retention_days = 1
   }
 
-  table_name = "test_table"
+  table_name  = "test_table"
   column_name = "first_test"
   colume_type = "VARIANT"
-  keys_list = ["first_test"]
+  keys_list   = ["first_test"]
 
-  fileformat_name = "test_name"
-  source_files_format_dictionary = {format_type = "CSV"}
-}  
+  fileformat_name                = "test_name"
+  source_files_format_dictionary = { format_type = "CSV" }
+}
